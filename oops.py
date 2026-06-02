966c2f070bb2bc6f394014d9ea801a2c99cef5e4
mohitjangra876 <mohitjangra876@gmail.com>
2026-06-02 20:54:56 +0530

    python oops

--- oops.py
+++ oops.py
@@ -82,3 +82,422 @@ print(p1.name, p1.age)
 print(p2.name, p2.age)
 
 
+#   -------  Python self parameter   -----------
+
+""" Importnat ->  self Does Not Have to Be Named "self"
+
+It does not have to be named self, you can call it whatever you like, but it has to be the first parameter of any method in the class:
+  """
+
+class Person:
+  def __init__(myobject, name, age):
+    myobject.name = name
+    myobject.age = age
+
+  def greet(abc):
+    print("Hello, my name is " + abc.name)
+
+p1 = Person("Emil", 36)
+p1.greet()
+
+
+""" 
+Calling Methods with self
+You can also call other methods within the class using self:
+"""
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    return "Hello, " + self.name
+
+  def welcome(self):
+    message = self.greet()
+    print(message + "! Welcome to our website.")
+
+p1 = Person("Tobias")
+p1.welcome()
+
+
+
+#   -----------   Python class properties ------------
+
+
+"""
+Modify Properties
+You can modify the value of properties on objects:
+"""
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Tobias", 25)
+print(p1.age)
+
+p1.age = 26
+print(p1.age)
+
+
+"""
+Delete Properties
+You can delete properties from objects using the del keyword:
+"""
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 30)
+
+del p1.age
+
+print(p1.name) # This works
+# print(p1.age) # This would cause an error
+
+
+"""
+Class Properties vs Object Properties
+Properties defined inside __init__() belong to each object (instance properties).
+
+Properties defined outside methods belong to the class itself (class properties) and are shared by all objects:
+"""
+
+class Person:
+  species = "Human" # Class property
+
+  def __init__(self, name):
+    self.name = name # Instance property
+
+p1 = Person("Emil")
+p2 = Person("Tobias")
+
+print(p1.name)
+print(p2.name)
+print(p1.species)
+print(p2.species)
+
+
+"""
+Modifying Class Properties
+When you modify a class property, it affects all objects:
+"""
+
+class Person:
+  lastname = ""
+
+  def __init__(self, name):
+    self.name = name
+
+p1 = Person("Linus")
+p2 = Person("Emil")
+
+Person.lastname = "Refsnes"
+
+print(p1.lastname)
+print(p2.lastname)
+
+
+"""
+Add New Properties
+You can add new properties to existing objects:
+"""
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+p1 = Person("Tobias")
+
+p1.age = 25
+p1.city = "Oslo"
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+
+
+#  -----------   Python Class Methods   -------------
+
+"""
+Class Methods
+Methods are functions that belong to a class. They define the behavior of objects created from the class.
+"""
+
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+
+
+""""
+Methods Modifying Properties
+Methods can modify the properties of an object:
+"""
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def celebrate_birthday(self):
+    self.age += 1
+    print(f"Happy birthday! You are now {self.age}")
+
+p1 = Person("Linus", 25)
+p1.celebrate_birthday()
+p1.celebrate_birthday()
+
+
+
+"""
+The __str__() Method
+The __str__() method is a special method that controls what is returned when the object is printed:
+"""
+
+#  without __str__
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+print(p1)
+
+#  with __str__
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} ({self.age})"
+
+p1 = Person("Tobias", 36)
+print(p1)
+
+
+#  example 2
+class Person:
+  def __str__(self):
+    return "This is new output"
+
+p1 = Person()
+print(p1)   
+
+
+
+# -----------------    Python  Inheritance   --------------------
+
+"""
+Create a Child Class
+To create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class:
+"""
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+
+
+#    Add the __init__() Function
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    #add properties etc.
+    pass
+
+#  The child's __init__() function overrides the inheritance of the parent's __init__() function.
+
+# To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+
+"""
+Use the super() Function
+Python also has a super() function that will make the child class inherit all the methods and properties from its parent:
+"""
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+"""   Add Properties    """    
+
+"""
+In the example below, the year 2019 should be a variable, and passed into the Student class when creating student objects. To do so, add another parameter in the __init__() function:
+"""
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+
+
+#  -----   Add Methods   -------
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+
+#   ---------------   Inheritance   ----------------------
+
+#  Inheritance Class Polymorphism
+
+
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang")       #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747")     #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
+
+
+#  -------------     Python  Encapslation   ----------------
+
+"""
+Encapsulation is about protecting data inside a class.
+
+It means keeping data (properties) and methods together in a class, while controlling how the data can be accessed from outside the class.
+
+This prevents accidental changes to your data and hides the internal details of how your class works.
+"""
+
+
+"""
+Private Properties
+In Python, you can make properties private by using a double underscore __ prefix:
+"""
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age # Private property
+
+p1 = Person("Emil", 25)
+print(p1.name)
+print(p1.__age) # This will cause an error
+
+
+#  Getter and setter
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+  def set_age(self, age):
+    if age > 0:
+      self.__age = age
+    else:
+      print("Age must be positive")
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
+
+
+"""
+Protected Properties
+Python also has a convention for protected properties using a single underscore _ prefix:
+"""
+
+"""
+Note: A single underscore _ is just a convention. It tells other programmers that the property is intended for internal use, but Python doesn't enforce this restriction.
+"""
+
+class Person:
+  def __init__(self, name, salary):
+    self.name = name
+    self._salary = salary # Protected property
+
+p1 = Person("Linus", 50000)
+print(p1.name)
+print(p1._salary) # Can access, but shouldn't
+
+
+
+#   Private Methods 
+
+
+class Calculator:
+  def __init__(self):
+    self.result = 0
+
+  def __validate(self, num):
+    if not isinstance(num, (int, float)):
+      return False
+    return True
+
+  def add(self, num):
+    if self.__validate(num):
+      self.result += num
+    else:
+      print("Invalid number")
+
+calc = Calculator()
+calc.add(10)
+calc.add(5)
+print(calc.result)
+# calc.__validate(5) # This would cause an error
